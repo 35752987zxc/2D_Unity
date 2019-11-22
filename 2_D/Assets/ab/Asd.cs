5799b41df14fe03079e670ba94d62e7e30a1c56e
35752987zxc <57181192+35752987zxc@users.noreply.github.com>
2019-11-22 21:12:59 +0800

    1122

---
+++ 2_D/Assets/ab/Asd.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Asd : MonoBehaviour
+{
+    public int a = 10, b = 3;
+
+    private void Start()
+    {
+        #region
+
+        print(a + b);
+        print(a - b);
+        print(a * b);
+        print(a / b);
+        print(a % b);
+        a = a + 1;
+        print(a);
+        print(a++);
+        print(++a);
+        a = a + 10;
+        a += 10;
+
+        #endregion
+
+
+        #region
+        print(a > b);
+        print(a < b);
+        print(a >= b);
+        print(a <= b);
+        print(a == b);
+        print(a != b);
+        #endregion
+        #region
+        print(true && false);  
+        print(true || false);
+
+
+        #endregion
+
+
+
+
+
+
+    }
+
+}
---
+++ 2_D/Assets/ab/Sif.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Sif : MonoBehaviour
+{
+    public bool gh;
+    public int scor;
+    public int combo = 1 ;
+    private void Start()
+    {
+        if (gh)
+        {
+            print(222);
+        }
+        else
+        {
+            print(111);
+        }
+
+        if (scor>=60)
+        {
+            print("ok");
+        }
+        else if (scor>=40)
+        {
+            print("ww");
+        }
+        else
+        {
+            print("ss");
+
+        }
+        if (combo >= 150)
+        {
+            print("攻擊 * 10");
+
+        }
+        else if (combo >= 100)
+        {
+            print("攻擊 * 5");
+
+        }
+        else if (combo >= 50)
+        {
+            print("攻擊 * 2");
+        }
+
+
+    }
+	
+        
+	
+}
